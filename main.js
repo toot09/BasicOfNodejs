816d4e7eef262defb771deb43cad6896828e11a6
ujone(Mac) <03toot28@gmail.com>
2021-09-05 13:43:57 +0900

    url

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function(request, response) {
-    var url = request.url;
-    if(request.url == '/') {
-        url = '/index.html';
+    
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+
+    console.log(queryData);
+    if(_url == '/') {
+        _url = '/index.html';
     }
-    if(request.url == '/favicon.ico') {
+    if(_url == '/favicon.ico') {
         return response.writeHead(404);
     }
     response.writeHead(200);
-    console.log(__dirname+url);
-    response.end(fs.readFileSync(__dirname+url));
+    console.log(__dirname+_url);
+    response.end(fs.readFileSync(__dirname+_url));
 });
 app.listen(3000);
---
+++ syntax/template.js
@@ -0,0 +1,8 @@
+var name = 'jone';
+var letters = 'hey my name is '+name+'. \nMy age is 33';
+console.log(letters);
+
+var literer = `ey my name is ${name}.
+My age is ${3+1}`;
+
+console.log(literer);
