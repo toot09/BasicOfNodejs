fe94e3b2b992a7f837931017ab14f5505466aae0
ujone(Mac) <03toot28@gmail.com>
2021-09-06 22:30:01 +0900

    정리

--- main.js
+++ main.js
@@ -5,41 +5,47 @@ var url = require('url');
 var app = http.createServer(function(request, response) {
     
     var _url = request.url;
-    var queryData = url.parse(_url, true).query;
+    var urlParse = url.parse(_url, true);
+    var queryData = urlParse.query;
+    var pathname = urlParse.pathname;
     var title = queryData.id;
-    var describe = "";
-    if(_url == '/') {
-        //_url = '/index.html';
-        title = 'Main';
-    }
-    if(_url == '/favicon.ico') {
-        return response.writeHead(404);
-    }
-    fs.readFile(`data/${title}`, 'utf8', (err,data)=>{
-        var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="index.html">WEB</a></h1>
-                    <ol>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ol>
-                    <h2>${title}</h2>
-                    <p>
-                    ${data}
-                    </p>
-                    </body>
-                    </html>
-                    `;
-        response.end(template);
-    });
-    response.writeHead(200);
     
+    //console.log(url.parse(_url, true));
+    if(pathname == "/") {
+        fs.readFile(`data/${title}`, 'utf8', (err,data)=>{
+            if(Object.keys(queryData).length==0) {
+                title = "Welcome";
+                data = "Hello Node.js";
+            }
+            var template = `
+                        <!doctype html>
+                        <html>
+                        <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                        </head>
+                        <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ol>
+                            <li><a href="/?id=HTML">HTML</a></li>
+                            <li><a href="/?id=CSS">CSS</a></li>
+                            <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        </ol>
+                        <h2>${title}</h2>
+                        <p>
+                        ${data}
+                        </p>
+                        </body>
+                        </html>
+                        `;
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
+    }
+
+     
 });
 app.listen(3000);
--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,7 +1,6 @@
 //console.log(process.argv[3]);
 pa = process.argv;
 var input1 = pa[2];
-var input2 = pa[3];
 
 if(input1%2 == 0) console.log("짝수임");
 if(input1%2 == 1) console.log("홀수임");
