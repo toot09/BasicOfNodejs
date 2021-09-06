4646582377087017cd7b0804a5d14f5798b8c627
ujone(Mac) <03toot28@gmail.com>
2021-09-06 21:40:19 +0900

    Arrange Main js using filesysstem.

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var title = queryData.id;
+    var describe = "";
     if(_url == '/') {
         //_url = '/index.html';
         title = 'Main';
@@ -14,9 +15,8 @@ var app = http.createServer(function(request, response) {
     if(_url == '/favicon.ico') {
         return response.writeHead(404);
     }
-
-    response.writeHead(200);
-    var template = `
+    fs.readFile(`data/${title}`, 'utf8', (err,data)=>{
+        var template = `
                     <!doctype html>
                     <html>
                     <head>
@@ -31,14 +31,15 @@ var app = http.createServer(function(request, response) {
                         <li><a href="/?id=JavaScript">JavaScript</a></li>
                     </ol>
                     <h2>${title}</h2>
-                    <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-                    <img src="coding.jpg" width="100%">
-                    </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
+                    <p>
+                    ${data}
                     </p>
                     </body>
                     </html>
                     `;
-
-    response.end(template);
+        response.end(template);
+    });
+    response.writeHead(200);
+    
 });
 app.listen(3000);
