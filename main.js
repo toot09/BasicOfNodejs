c103dbe86ae74a93a786f734641abcca36b2e511
ujone(Mac) <03toot28@gmail.com>
2021-09-11 22:01:17 +0900

    Add getTemplateHTML function.

--- main.js
+++ main.js
@@ -12,13 +12,7 @@ var app = http.createServer(function(request, response) {
     
     //console.log(url.parse(_url, true));
     //console.log(fs.readdirSync('data/'));
-    var files = fs.readdirSync('data/');
-    var fileList = "";
-    for(var i=0; i<files.length; i++) {
-        fileList += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`
-    }
     
-
     if(pathname == "/") {
         fs.readFile(`data/${title}`, 'utf8', (err,data)=>{
             // queryData length가 0 이라는건 query값이 없다는것 즉 main 페이지이기 때문에 welcome으로함
@@ -26,25 +20,7 @@ var app = http.createServer(function(request, response) {
                 title = "Welcome";
                 data = "Hello Node.js";
             }
-            var template = `
-                        <!doctype html>
-                        <html>
-                        <head>
-                        <title>WEB1 - ${title}</title>
-                        <meta charset="utf-8">
-                        </head>
-                        <body>
-                        <h1><a href="/">WEB</a></h1>
-                        <ol>
-                            ${fileList}
-                        </ol>
-                        <h2>${title}</h2>
-                        <p>
-                        ${data}
-                        </p>
-                        </body>
-                        </html>
-                        `;
+            var template =  getTemplateHTML(title, data);
             response.writeHead(200);
             response.end(template);
         });
@@ -52,7 +28,35 @@ var app = http.createServer(function(request, response) {
         response.writeHead(404);
         response.end('Not found');
     }
-
-     
 });
+
+
+function getTemplateHTML(title, data) {
+    var files = fs.readdirSync('data/');
+    var fileList = "";
+    for(var i=0; i<files.length; i++) {
+        fileList += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`
+    }
+
+    return `<!doctype html>
+            <html>
+            <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                ${fileList}
+            </ol>
+            <h2>${title}</h2>
+            <p>
+            ${data}
+            </p>
+            </body>
+            </html>
+            `;
+
+}
+
 app.listen(3000);
